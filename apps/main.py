d3cfa01201d5d8d1b86489d20af67b665b21a06c
Iniyan <78460090+iniyanv@users.noreply.github.com>
2021-12-11 19:37:35 +0530

    rebuild

--- apps/main.py
+++ apps/main.py
@@ -1,58 +1,64 @@
+from kivy.app import App 
 
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.event import EventDispatcher
+from kivy.uix.label import Label 
+
 from kivy.network.urlrequest import UrlRequest
-from kivy.properties import ListProperty
-from kivy.clock import Clock
+
+from kivy.properties import StringProperty
+
+from kivy.lang import Builder
+
 
 kv = """
+
 BoxLayout:
+
     orientation: 'vertical'
-    AsyncImage:
-        id: image
-        source: "https://random.dog/1ae6411b-8f81-438a-a793-7642a3e61128.jpg"
+
+    Label:
+        id : text
+
+        text: 'hello world'
+
+    TextInput:
+        id: input
+
+        hint_text: 'Paste url'
+        on_text: app.process()
     Button:
-        size_hint_y: None
-        height: 48
-        text: 'Get Dog Image'
-        on_release: app.aw.check_net()
+        text: 'shortern'
+        on_release: app.process()
+
 """
 
 
-class AccessWeb(EventDispatcher):
-    facts = ListProperty()
+class Shortner():
 
-    def check_net(self):
-        UrlRequest('https://random.dog/woof', on_success=self.net_success)
+    def check_net(self, long_url):
 
-    def net_success(self,req, r):
-        print(f'Success: {req.is_finished}')
-        self.dog.clear()
-        self.dog.append(f"https://random.dog/{r}")
-        print(str(r))
-        print(self.dog)
-        app = App.get_running_app()
-        app.root.ids.image.source = self.facts[0]
-
-
-class TestAccessWebApp(App):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cnt = 0
-        self.aw = None
-
+        UrlRequest(f'https://tinyurl.com/api-create.php?url={long_url}', on_success=self.net_success)
+        
+    def net_success(self, req, r):
+        print(r)
+        
+inputtext = ""
+class Main(App):
     def build(self):
-        self.aw = AccessWeb()
         return Builder.load_string(kv)
 
-    def on_start(self):
-        Clock.schedule_interval(self.display_dog_image, 5)
+    def process(self):
+        inputtext = self.root.ids.input.text
+        sh = Shortner().check_net(inputtext)
+        # print(self.Shortner().shorturl)
+        # self.root.ids.text.text = str(shorturl)
+# test = Quote()
 
-    def display_dog_image(self, dt):
-        if self.aw.dog:
-            self.root.ids.image.source = self.aw.dog[0]
+# test.check_net()
 
+# print(test.quote['text'])
+print(inputtext)
 
-TestAccessWebApp().run()
+if __name__ == "__main__":
+
+    Main().run()
+
